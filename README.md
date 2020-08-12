e8f671846383eaf9bc0c3719f1ee46b2b847ffa5
Shingo Inoue <leko.noor@gmail.com>
2020-08-12 17:11:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# dotfiles
+My dotfiles for GitHub Codespaces
+
+> &mdash; [Personalizing Codespaces for your account - GitHub Docs](https://docs.github.com/en/github/developing-online-with-codespaces/personalizing-codespaces-for-your-account)
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -eu
+
+install_golang () {
+  local go_version=$1
+  if [[ "$(uname)" == 'Linux' ]]; then
+    platform='linux'
+  else
+    platform='darwin'
+  fi
+  wget https://golang.org/dl/go$go_version.$platform-amd64.tar.gz
+  tar -C /usr/local -xzf go$go_version.$platform-amd64.tar.gz
+  export PATH=$PATH:/usr/local/go/bin
+}
+
+install_peco() {
+  if [[ "$(uname)" == 'Linux' ]]; then
+    sudo apt install -y peco
+  else
+    brew install peco
+  fi
+}
+
+# Configuration
+GO_VERSION=1.15
+
+# Install Golang
+# https://golang.org/doc/install#install
+install_golang $GO_VERSION
+
+# Install Rust
+# https://forge.rust-lang.org/infra/other-installation-methods.html#other-ways-to-install-rustup
+curl https://sh.rustup.rs -sSf | sh
+
+# Install Deno
+# https://deno.land/#installation
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+# Install ghq
+# https://github.com/x-motemen/ghq#go-get
+go get github.com/x-motemen/ghq
+
+# Install peco
+# https://golang.org/doc/install#install
+install_peco
+
+# Install bash completions
+prefix=/usr/share/bash-completion/completions/
+completions=(apt autoconf c++ chmod chown chrome chromium chromium-browser curl dmesg export find jq kill lsof make ping pkg-config pwd rsync scp snap ssh ssh-keygen sudo systemctl tar wget)
+for item in ${completions[@]}; do
+  if [ -f "$prefix/$item" ]; then
+    source "$prefix/$item"
+  fi
+done
